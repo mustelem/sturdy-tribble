10695aa26a8b45043d5177249bf964c8e5ef4fc4
Baris Hasdemir <baris@hasdemir.com>
2024-06-22 23:22:32 -0400

    Runtime beats 90%, memory beats 100%

--- 383-Ransom Note/main.go
+++ 383-Ransom Note/main.go
@@ -1,29 +1,19 @@
 package main
 
 func canConstruct(ransomNote string, magazine string) bool {
-	magArchive := map[rune]int{}
+	letters := [26]int{}
 
 	for _, v := range magazine {
-		count, e := magArchive[v]
-		if e {
-			magArchive[v] = count + 1
-		} else {
-			magArchive[v] = 1
-		}
+		order := v % 'a'
+		letters[order]++
 	}
 
 	for _, v := range ransomNote {
-		count, e := magArchive[v]
-
-		if e {
-			if count == 0 {
-				return false
-			} else {
-				magArchive[v] = count - 1
-			}
-		} else {
+		order := v % 'a'
+		if letters[order] == 0 {
 			return false
 		}
+		letters[order]--
 	}
 
 	return true
