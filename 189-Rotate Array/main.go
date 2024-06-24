4460cab6502b7945f7f0bea188f205e60520a65d
Baris Hasdemir <baris@hasdemir.com>
2024-06-24 11:19:05 -0400

    Another solution. 32ms 9 MB

--- 189-Rotate Array/main.go
+++ 189-Rotate Array/main.go
@@ -12,19 +12,9 @@ func rotate(nums []int, k int) {
 	if k > len(nums) {
 		k = k % len(nums)
 	}
+	cutoff := len(nums) - k
 
-	cutoff := len(nums) - k // 4 = 7 - 3
-
-	temp := make([]int, k)
-	copy(temp, nums[cutoff:]) // temp = {5,6,7}
-
-	for i := cutoff - 1; i >= 0; i-- {
-		nums[i+k] = nums[i]
-	} // nums = {1,2,3,1,2,3,4}
-
-	for i, _ := range temp {
-		nums[i] = temp[i]
-	}
+	copy(nums, append(nums[cutoff:], nums[:cutoff]...))
 
 	fmt.Println(nums)
 }
