cb0b249ba0e02b478729a17ffaacaa8875ef48ee
Baris Hasdemir <baris@hasdemir.com>
2024-06-24 11:08:33 -0400

    Runtime beats 57%, mem beats 77%

--- 189-Rotate Array/main.go
+++ 189-Rotate Array/main.go
@@ -3,14 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	rotate([]int{8, 9, 1, 10, 5, 2}, 3)
-	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 3)
-	rotate([]int{-1, -100, 3, 99}, 2)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 10)
 }
 
 func rotate(nums []int, k int) {
 	fmt.Println(nums)
-	cutoff := len(nums) - k
-	nums = append(nums[cutoff:], nums[:cutoff]...)
+
+	if k > len(nums) {
+		k = k % len(nums)
+	}
+
+	cutoff := len(nums) - k // 4 = 7 - 3
+
+	temp := make([]int, k)
+	copy(temp, nums[cutoff:]) // temp = {5,6,7}
+
+	for i := cutoff - 1; i >= 0; i-- {
+		nums[i+k] = nums[i]
+	} // nums = {1,2,3,1,2,3,4}
+
+	for i, _ := range temp {
+		nums[i] = temp[i]
+	}
+
 	fmt.Println(nums)
 }
