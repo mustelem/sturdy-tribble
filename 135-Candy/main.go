ed62cbbaeb104af7563e936f22c5fe91578dc3e4
Baris Hasdemir <baris@hasdemir.com>
2024-07-18 16:00:17 -0400

    Wrong solution

--- 135-Candy/main.go
+++ 135-Candy/main.go
@@ -1,5 +1,47 @@
 package main
 
 func candy(ratings []int) int {
+	var retVal int
+	var backtick int
 
+	awards := make([]int, len(ratings))
+	awards[0]++
+
+	for i := 1; i < len(ratings); i++ {
+		awards[i]++
+		if ratings[i] == ratings[i-1] {
+			continue
+		}
+		if ratings[i] < ratings[i-1] {
+			if i == len(ratings)-1 {
+				j := 0
+				for j <= backtick {
+					if ratings[i-j] < ratings[i-j-1] {
+						awards[i-j-1] = awards[i-j] + 1
+					}
+					j++
+				}
+				backtick = 0
+			} else {
+				backtick++
+			}
+
+		} else {
+			j := 0
+			for j < backtick {
+				if ratings[i-j-1] < ratings[i-j-2] {
+					awards[i-j-2] = awards[i-j-1] + 1
+				}
+				j++
+			}
+			backtick = 0
+			awards[i] = awards[i-1] + 1
+		}
+	}
+
+	for _, v := range awards {
+		retVal += v
+	}
+
+	return retVal
 }
--- 135-Candy/main_test.go
+++ 135-Candy/main_test.go
@@ -26,6 +26,41 @@ func Test_candy(t *testing.T) {
 			},
 			want: 4,
 		},
+		{
+			name: "failed test 1",
+			args: args{
+				ratings: []int{1, 2, 87, 87, 87, 2, 1},
+			},
+			want: 13,
+		},
+		{
+			name: "failed test 2",
+			args: args{
+				ratings: []int{29, 51, 87, 87, 72, 12},
+			},
+			want: 12,
+		},
+		{
+			name: "failed test 3",
+			args: args{
+				ratings: []int{1, 3, 4, 5, 2},
+			},
+			want: 11,
+		},
+		{
+			name: "my test 1",
+			args: args{
+				ratings: []int{1, 5, 4, 3, 2},
+			},
+			want: 11,
+		},
+		{
+			name: "my test 2",
+			args: args{
+				ratings: []int{2, 3, 4, 5, 2, 3},
+			},
+			want: 13,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
