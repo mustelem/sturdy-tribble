d0334d5a0fcb95232293aeac1fa29e8827e1887b
Baris Hasdemir <baris@hasdemir.com>
2024-09-24 14:47:23 -0400

    misc update

--- 42-Trapping Rain Water/main.go
+++ 42-Trapping Rain Water/main.go
@@ -1,6 +1,20 @@
 package main
 
 func trap(height []int) int {
+	var tempAcc, permAcc int
+	var maxHeight int
+
+	for _, v := range height {
+		if v < maxHeight {
+			tempAcc += maxHeight - v
+		} else if v > 0 {
+
+		} else {
+			permAcc += tempAcc
+			tempAcc = 0
+		}
+		maxHeight = max(v, maxHeight)
+	}
 
 	return 0
 }
