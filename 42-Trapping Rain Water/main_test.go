9dbec4526b3908b4171583a50dfe72c7f93ebcec
Baris Hasdemir <baris@hasdemir.com>
2024-12-12 10:19:37 -0500

    initial commit

--- 42-Trapping Rain Water/main_test.go
+++ 42-Trapping Rain Water/main_test.go
@@ -11,20 +11,49 @@ func Test_trap(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
 		{
-			name: "Ex 1",
+			name: "Should hold 1 unit of water",
+			args: args{
+				height: []int{1, 0, 1},
+			},
+			want: 1,
+		}, {
+			name: "Should hold 1 unit of water",
+			args: args{
+				height: []int{1, 0, 2},
+			},
+			want: 1,
+		}, {
+			name: "Should hold 1 unit of water",
+			args: args{
+				height: []int{2, 0, 1},
+			},
+			want: 1,
+		}, {
+			name: "Should hold 2 unit of water",
+			args: args{
+				height: []int{2, 0, 2},
+			},
+			want: 2,
+		}, {
+			name: "Should hold 3 unit of water",
+			args: args{
+				height: []int{2, 0, 1, 2},
+			},
+			want: 3,
+		}, {
+			name: "Should hold 6 unit of water",
 			args: args{
 				height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1},
 			},
 			want: 6,
 		}, {
-			name: "Ex 2",
+			name: "Should hold 9 unit of water",
 			args: args{
 				height: []int{4, 2, 0, 3, 2, 5},
 			},
 			want: 9,
-		}, {}, {}, {}, {}, {},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
