2bbf036253d967537143c3346b940f02fae8cee7
Baris Hasdemir <baris@hasdemir.com>
2024-12-18 21:54:29 -0500

    runtime beats 100%, memory usage beats 50%

--- 151-Reverse Words in a String/main.go
+++ 151-Reverse Words in a String/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"strings"
 )
 
 func reverseWords(s string) string {
-	var retVal string
 	var words []string = strings.Split(s, " ")
+	var buffer bytes.Buffer
 
 	for i := len(words) - 1; i >= 0; i-- {
 		if words[i] != "" {
-			retVal += words[i]
-			retVal += " "
+			buffer.WriteString(words[i])
+			buffer.WriteString(" ")
 		}
 	}
 
-	return strings.TrimSpace(retVal)
+	return strings.TrimSpace(buffer.String())
 }
