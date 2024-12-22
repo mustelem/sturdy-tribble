1b6bdd0146ed989bc6e0e45adbce52ed715a22e3
Baris Hasdemir <baris@hasdemir.com>
2024-12-21 19:58:08 -0500

    initial solution

--- 6-Zigzag Conversion/main.go
+++ 6-Zigzag Conversion/main.go
@@ -1,5 +1,80 @@
 package main
 
-func convert(s string, numRows int) string {
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
+func convert(s string, numRows int) string {
+	var activeWidth int
+	letters := strings.Split(s, "")
+
+	L := len(letters)
+
+	dist := max(numRows-2, 0)
+	quot1 := L / (numRows + dist)
+	rem1 := L % (numRows + dist)
+	quot2 := rem1 / numRows
+	rem2 := rem1 % numRows
+
+	w := quot1*(dist+1) + quot2 + rem2
+
+	var table [][]string = make([][]string, numRows)
+
+	for i := range table {
+		table[i] = make([]string, w) // TODO Not sure about the width, yet.
+	}
+
+	i := 0
+	j := 0
+	letterPtr := 0
+
+	down := true
+	for {
+		table[i][j] = letters[letterPtr]
+		letterPtr++
+		if letterPtr > len(letters)-1 {
+			activeWidth = j
+			break
+		}
+
+		if down {
+			i++
+			if i > numRows-1 {
+				if i-2 < 0 {
+					i = 0
+				} else {
+					i = i - 2
+					down = false
+				}
+				j++
+			}
+		} else {
+			i--
+			j++
+			if i < 0 {
+				down = true
+				j--
+				i = i + 2
+			}
+		}
+	}
+
+	var buffer bytes.Buffer
+	for i := 0; i < numRows; i++ {
+		for j := 0; j <= activeWidth; j++ {
+			if table[i][j] != "" {
+				buffer.WriteString(table[i][j])
+			}
+		}
+	}
+
+	return buffer.String()
+}
+
+func main() {
+	for i := 3; i < 9; i++ {
+		fmt.Println(convert("PAYPALISHIRING", i))
+	}
 }
--- 6-Zigzag Conversion/main_test.go
+++ 6-Zigzag Conversion/main_test.go
@@ -12,7 +12,12 @@ func Test_convert(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
+		{name: "Ex01", args: args{s: "PAYPALISHIRING", numRows: 3}, want: "PAHNAPLSIIGYIR"},
+		{name: "Ex02", args: args{s: "PAYPALISHIRING", numRows: 4}, want: "PINALSIGYAHRPI"},
+		{name: "Ex03", args: args{s: "A", numRows: 1}, want: "A"},
+		{name: "Ex04", args: args{s: "AB", numRows: 1}, want: "AB"},
+		{name: "Ex05", args: args{s: "ABC", numRows: 1}, want: "ABC"},
+		{name: "ExLong", args: args{s: "ssartgskkqomcjiaxzgnhfljxmsudswvlxogfgsqygebsmbpoiexpqhmebhhufehespkahcyngbhudzindgevprzqaikfotkiiwkpyjfgmoapnjetrjogcqweajjrevzntkervlzhaaznlficqziupgyrrkiwfkjzwpsrbsabszqfhzhxarspzqirctpifajbpbusvutpwmudnbcnuweuponrhczmckntmjmjehzattfixjvrgbnvqamxcomgybcmlowpvvrrqyznhxfnyskotzoxnagnbicoyafvvymnwobglgtlagcvfqvssbhvljkjjpegotukhvsudohdujbzbwttxcpkmztxqzeesarbxjxaxfftqgsscnlbqclcbebsgfyyhpcebzgagmuxuopxccasfmwisxcyfbzmsdtvezwlnqiiezhibhaivyroytoduprpukkzmgkzyuhdtolwyoftmwjmpapmrjbmvllhsxhsrqvkhjgfznynjkabkrnbaonybafvxooohlaoujtvwtjifjjpawtdmgbpjilgzbxgmxujipehkppqyyhbwaekjhzspmaqpxwexsnfjtmujbmhbvkxwqjhxlbpzbfpzctwwibgbqcmrqwvlgsjxesuptlqvrhuvasrktzmldydtwyhdsqaylwpekgzbnvyqnrajrouupxqlxxospqqapgfzmgcbccrptsymitjxszjswzknqaqhgviudkwfmpxhjvusqdpjcadaanpsnfzwchsgtqlhikorgijylbjpbmrdzhxpmwnpffvayihgtyxbgjzumllpxdtxkqowpbnwikzgtioogoppxqljbwybbtanmomdrzzzkyifjytpmkejcrueovhrohfavrdmqfncfxhowcgimmupeovulclalqcghiuaphcwfkndmtlbfhsjypdjtrlehokrygrpnvluhyxutlxzspkqgqczhndqdphbvaskwghfeezyr", numRows: 155}, want: "svpcscffzhwagqbcpfravztaktlspwungtsbwidsgblihmklhxbgtkgvhgclqbljbqboojqqlfmwkwcahcnjxmlsjmjkrcjiypvqlyavebwupxvghvvdzfomlojgatbgetnyujsprhokujulfchmxmelivtemhjbsjsioxnufugkmgdnpcrsaostwyunhxlogdxdeqhrsouwvxpwzjxrfnvtbphcvlozqunlxkbavfuoswmaqhgytpsmyfntsrdxgfxzkrusxchtvtqhpjzalynkkmfxgzmelhzeyzadosbqtwyrpsrxbdhkmrqhtvqbvzywogpveyyeqopeqhuciwspdrzeoapschxlrkqjnpmbhaedqcxeykqhbjplmdmyxiwppegajpthbmxueybhofxkjvhcfmgfauxtgzisfmqxbykeagbnkwhqszvzgevsgyzhsnclqzfpbninrekgljrdearbpamzhvqbjoycjcgrmryxlmonnicduagfbtutbtewpbhtbaxbuaspqydzgjlwzhfjxbieyfxjnjyioldmhjsqgjptpxemcwqpvtevqppnbtaorkzjpgzcgugoqmaofoazgaziicmlmtkhuhggfrxoczonuobktooocikppxcwiuxvrniecfpbwwcatpkuabswpnsyyoycfnmqjbmoikfnwatxgdibjtmusnxdomxrsxawckzpppybjlntfasljubkwmevzjzutsmnkzrusynjjbdnqgoptzabgbvfqxcjeghyqazjgtwfwhvgeilkihapnvuijtqqdyjcirkarriswveiehffvqzxmfzzhsppnpihxntsblhwkrhljmeaavvprximuxvhvbshlzyjqzzhrrddhfompraqypjmaztacbzsopapnbdmdjlaujabfspwalibrmnycrptpjqsufsizpkongiwkyfruzzwzopjmlwkgkgociyfkthhrwzdslriyhgqkut"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
