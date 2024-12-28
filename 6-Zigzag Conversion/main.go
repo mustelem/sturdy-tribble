1e16b011f55d6579c84810cc049c9c38986eb4fe
Baris Hasdemir <baris@hasdemir.com>
2024-12-27 20:29:26 -0500

    Better solution

--- 6-Zigzag Conversion/main.go
+++ 6-Zigzag Conversion/main.go
@@ -7,65 +7,28 @@ import (
 )
 
 func convert(s string, numRows int) string {
-	var activeWidth int
+	var buffer bytes.Buffer
 	letters := strings.Split(s, "")
 
-	L := len(letters)
-
-	dist := max(numRows-2, 0)
-	quot1 := L / (numRows + dist)
-	rem1 := L % (numRows + dist)
-	quot2 := rem1 / numRows
-	rem2 := rem1 % numRows
-
-	w := quot1*(dist+1) + quot2 + rem2
-
-	var table [][]string = make([][]string, numRows)
-
-	for i := range table {
-		table[i] = make([]string, w) // TODO Not sure about the width, yet.
-	}
-
 	i := 0
-	j := 0
-	letterPtr := 0
+	width := numRows + max(0, numRows-2)
+	diff := width
 
-	down := true
-	for {
-		table[i][j] = letters[letterPtr]
-		letterPtr++
-		if letterPtr > len(letters)-1 {
-			activeWidth = j
-			break
+	for j := 0; j < numRows; j++ {
+		i = j
+		buffer.WriteString(letters[i])
+
+		diff = width - max(0, 2*j)
+		if diff == 0 {
+			diff = width - diff
 		}
+		for i+diff < len(letters) {
+			buffer.WriteString(letters[i+diff])
+			i += diff
 
-		if down {
-			i++
-			if i > numRows-1 {
-				if i-2 < 0 {
-					i = 0
-				} else {
-					i = i - 2
-					down = false
-				}
-				j++
-			}
-		} else {
-			i--
-			j++
-			if i < 0 {
-				down = true
-				j--
-				i = i + 2
-			}
-		}
-	}
-
-	var buffer bytes.Buffer
-	for i := 0; i < numRows; i++ {
-		for j := 0; j <= activeWidth; j++ {
-			if table[i][j] != "" {
-				buffer.WriteString(table[i][j])
+			diff = width - diff
+			if diff == 0 {
+				diff = width - diff
 			}
 		}
 	}
@@ -74,7 +37,5 @@ func convert(s string, numRows int) string {
 }
 
 func main() {
-	for i := 3; i < 9; i++ {
-		fmt.Println(convert("PAYPALISHIRING", i))
-	}
+	fmt.Println(convert("PAYPALISHIRING", 6))
 }
