e324601fe7192c02936a0605c0ce3d65ca053520
Baris Hasdemir <baris@hasdemir.com>
2025-01-17 13:36:20 -0500

    Initial commit

---
+++ 49-Group Anagrams/go.mod
@@ -0,0 +1,3 @@
+module baris/lc/49
+
+go 1.23.5
---
+++ 49-Group Anagrams/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_groupAnagrams(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		{name: "Ex01", args: args{strs: []string{"eat", "tea", "tan", "ate", "nat", "bat"}}, want: [][]string{{"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}}},
+		{name: "Ex02", args: args{strs: []string{""}}, want: [][]string{{""}}},
+		{name: "Ex03", args: args{strs: []string{"a"}}, want: [][]string{{"a"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := groupAnagrams(tt.args.strs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("groupAnagrams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
