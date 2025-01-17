347b2b96ce3886054cc47df63f9990d40b34b690
Baris Hasdemir <baris@hasdemir.com>
2025-01-17 16:50:59 -0500

    Beats 100% in runtime and 83% in space

--- 104-Maximum Depth of Binary Tree/main.go
+++ 104-Maximum Depth of Binary Tree/main.go
@@ -8,3 +8,25 @@ package main
  *     Right *TreeNode
  * }
  */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return 1
+	} else if root.Left == nil {
+		return maxDepth(root.Right) + 1
+	} else if root.Right == nil {
+		return maxDepth(root.Left) + 1
+	}
+
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
