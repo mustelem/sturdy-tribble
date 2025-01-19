12a22c87c3bc86c509a9237616a1f2348d643713
Baris Hasdemir <baris@hasdemir.com>
2025-01-18 22:04:00 -0500

    Beats 100% runtime

--- 2-Add Two Numbers/main.go
+++ 2-Add Two Numbers/main.go
@@ -7,6 +7,58 @@ package main
  *     Next *ListNode
  * }
  */
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	stack := make([]ListNode, 0)
+
+	var atHand int
+	for {
+
+		if l1 == nil && l2 == nil {
+			if atHand == 1 {
+				stack = append(stack, ListNode{1, nil})
+				break
+			}
+
+			break
+		}
+
+		var res int
+		var left int
+		var right int
+
+		if l1 != nil {
+			left = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			right = l2.Val
+			l2 = l2.Next
+		}
+
+		res = left + right + atHand
+		if res > 9 {
+			atHand = 1
+			res = res - atHand*10
+		} else {
+			atHand = 0
+		}
+
+		stack = append(stack, ListNode{res, nil})
+	}
+
+	root := stack[len(stack)-1]
+	for i := len(stack) - 2; i >= 0; i-- {
+		child := root
+		root = stack[i]
+		root.Next = &child
+	}
+
+	return &root
 }
